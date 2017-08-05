93869c7ac1b5476dc0c886c7e5c34fd3985e77d2
Edward Dupre <edwd42@gmail.com>
2017-08-05 15:09:16 +0000

    finish video 12 static maps and street view

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,9 @@
 // Create a map variable
 var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+      
 // Function to initialize the map within the map div
 function initMap() {
     var styles = [
@@ -25,7 +29,7 @@ function initMap() {
             featureType: 'road.highway',
             elementType: 'geometry.stroke',
             stylers: [
-              { color: '#efe9e4' },
+              { color: '#ffff00' },
               { lightness: -40 }
             ]
           },{
@@ -63,71 +67,154 @@ function initMap() {
             featureType: 'road.highway',
             elementType: 'geometry.fill',
             stylers: [
-              { color: '#efe9e4' },
+              { color: '#ffff00' },
               { lightness: -25 }
             ]
           }
         ];
 
+        // Constructor creates a new map - only center and zoom are required.
+        // center at Prov City Hall
         map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: 40.74135, lng: -73.99802},
+         center: {lat: 41.824023, lng: -71.412908},
          zoom: 14,
          styles: styles,
          mapTypeControl: false
-
-    });
-    // Create a single latLng literal object.
-    var singleLatLng = {lat: 40.74135, lng: -73.99802};
-    // TODO: Create a single marker appearing on initialize -
-    // Create it with the position of the singleLatLng,
-    // on the map, and give it your own title!
-    var marker = new google.maps.Marker({
-        position: singleLatLng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'My First Marker!'
-    });
-    
-    // TODO: create a single infowindow, with your own content.
-    // It must appear on the marker
-    var largeInfowindow = new google.maps.InfoWindow({
-        content: 'Where\'s Waldo?'
-    });
-    
-    // TODO: create an EVENT LISTENER so that the infowindow opens when
-    // the marker is clicked!
-    marker.addListener('click', function() {
-        // populateInfoWindow(this, largeInfowindow);
-        largeInfowindow.open(map, marker);
-    });
-    
-    // Style the markers a bit. This will be our listing marker icon.
-    var defaultIcon = makeMarkerIcon('0091ff');
-
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('FFFF24');
+        });
         
-    // Two event listeners - one for mouseover, one for mouseout,
-    // to change the colors back and forth.
-    marker.addListener('mouseover', function() {
-        this.setIcon(highlightedIcon);
-    });
-    marker.addListener('mouseout', function() {
-        this.setIcon(defaultIcon);
-  });
-  
-    // This function takes in a COLOR, and then creates a new marker
-    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-    // of 0, 0 and be anchored at 10, 34).
-    function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21,34));
-    return markerImage;
-    }
-}
+        // Create a single latLng literal object.
+        // WaterFire Building
+        // var singleLatLng = {lat: 41.828826, lng: -71.433083};
+        
+        // These are the real estate listings that will be shown to the user.
+        // Normally we'd have these in a database instead.
+        var locations = [
+            {title: 'Upstart', location: {lat: 41.823941, lng: -71.41183}},
+            {title: 'URI Providence Campus', location: {lat: 41.823182, lng: -71.413836}},
+            {title: 'WaterFire Building', location: {lat: 41.828866, lng: -71.433116}},
+            {title: '247 Hope Street', location: {lat: 41.832092, lng: -71.399835}},
+            {title: 'CIT Building, Brown University', location: {lat: 41.827051, lng: -71.399599}},
+            {title: 'Kabob and Curry', location: {lat: 41.828778, lng: -71.400533}}
+        ];
+
+        var largeInfowindow = new google.maps.InfoWindow();
+
+
+        // Style the markers a bit. This will be our listing marker icon.
+        var defaultIcon = makeMarkerIcon('0091ff');
+
+        // Create a "highlighted location" marker color for when the user
+        // mouses over the marker.
+        var highlightedIcon = makeMarkerIcon('FFFF24');
+
+        // The following group uses the location array to create an array of markers on initialize.
+        for (var i = 0; i < locations.length; i++) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
+          var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open the large infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+          // Two event listeners - one for mouseover, one for mouseout,
+          // to change the colors back and forth.
+          marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+          });
+          marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+          });
+        }
+
+        document.getElementById('show-listings').addEventListener('click', showListings);
+        document.getElementById('hide-listings').addEventListener('click', hideListings);
+
+      }
+
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          // Clear the infowindow content to give the streetview time to load.
+          infowindow.setContent('');
+          infowindow.marker = marker;
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+          var streetViewService = new google.maps.StreetViewService();
+          var radius = 50;
+          // In case the status is OK, which means the pano was found, compute the
+          // position of the streetview image, then calculate the heading, then get a
+          // panorama from that and set the options
+          function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+              var nearStreetViewLocation = data.location.latLng;
+              var heading = google.maps.geometry.spherical.computeHeading(
+                nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                var panoramaOptions = {
+                  position: nearStreetViewLocation,
+                  pov: {
+                    heading: heading,
+                    pitch: 30
+                  }
+                };
+              var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), panoramaOptions);
+            } else {
+              infowindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>No Street View Found</div>');
+            }
+          }
+          // Use streetview service to get the closest streetview image within
+          // 50 meters of the markers position
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+          // Open the infowindow on the correct marker.
+          infowindow.open(map, marker);
+        }
+      }
+
+      // This function will loop through the markers array and display them all.
+      function showListings() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+
+      // This function will loop through the listings and hide them all.
+      function hideListings() {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      }
+
+      // This function takes in a COLOR, and then creates a new marker
+      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+      // of 0, 0 and be anchored at 10, 34).
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+      }
